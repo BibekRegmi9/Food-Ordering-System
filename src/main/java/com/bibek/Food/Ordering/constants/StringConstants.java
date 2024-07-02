e56d020ef9810b58b87bd6db9cc87ceb84ee1fc4
Bibek Regmi <vivekregmi83@gmail.com>
2024-07-02 22:10:54 +0545

    feat: string constants added

---
+++ src/main/java/com/bibek/Food/Ordering/constants/StringConstants.java
@@ -0,0 +1,46 @@
+package com.bibek.Food.Ordering.constants;
+
+public final class StringConstants {
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DATE_FORMAT_WITH_TIME = "yyyy-MM-dd HH:mm:ss";
+    public static final String USER = "user";
+    public static final String INTERNAL_USER_NOT_FOUND = "Internal User Not found ";
+    public static final String INTERNAL_USER_AUTH_ERROR = "Internal User Auth Error";
+    public static final String BASIC_AUTHENTICATION = "Basic ";
+    public static final String LOGIN_URI = "/login";
+    public static final String ACTIVATED_ID = "Activated id: ";
+    public static final String DATE_FORMAT_ISO = "yyyy-MM-dd'T'HH:mm:ss";
+
+    public static final String SUCCESS_SAVE = "success.save";
+    public static final String MESSAGE_SENT = "message.sent";
+    public static final String SUCCESS_RETRIEVE = "success.retrieve";
+    public static final String SUCCESS_UPDATE = "success.update";
+    public static final String SUCCESS_UPDATE_MODULE = "success.update.module";
+    public static final String MESSAGE_READ = "message.read";
+    public static final String SUCCESS_RESEND = "success.resend";
+    public static final String SUCCESS_DELETE = "success.delete";
+    public static final String SUCCESS_EMAIL_SEND = "success.email.send";
+    public static final String SUCCESS_PASSWORD_CHANGE = "success.password.changed";
+    public static final String SUCCESS_TOGGLE = "success.toggle";
+    public static final String SUCCESS_GENERATE = "success.generate";
+    public static final String CUSTOM = "success.custom";
+    public static final String SUCCESS_SAVE_DRAFT = "success.save.draft";
+    public static final String SUCCESS_UPDATE_DRAFT = "success.update.draft";
+    public static final String DATA_EMPTY = "data.empty";
+    public static final String NOT_FOUND = "not.found";
+    public static final String USER_NOT_FOUND = "error.user.not.found";
+    public static final String INVALID_CREDENTIALS = "incorrect.username.password";
+    public static final String ACCOUNT_NOT_VERIFIED = "account.not.verified";
+    public static final String SUPER_ADMIN = "SUPER_ADMIN";
+    public static final String INTERNAL_SERVER_ERROR = "internal.server.error";
+    public static final String EMPTY_STRING = "";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String SUCCESS_IMPORT = "success.import";
+    public static final String IMPORT = "import";
+    public static final String LOGS = "logs";
+    public static final String LOGS_TYPES = "logs.types";
+    public static final String INVALID_FILE_FORMAT = "invalid.file.format";
+    public static final String COLON = ":";
+    public static final String REQUESTED_DATE = "requestedDate";
+}
+
