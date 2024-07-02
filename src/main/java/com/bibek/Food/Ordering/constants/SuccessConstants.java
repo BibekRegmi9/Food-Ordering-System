2346f902260e48e7977234fe6e92f0215b2bc162
Bibek Regmi <vivekregmi83@gmail.com>
2024-07-02 21:43:54 +0545

    feat: constants added

---
+++ src/main/java/com/bibek/Food/Ordering/constants/SuccessConstants.java
@@ -0,0 +1,27 @@
+package com.bibek.Food.Ordering.constants;
+
+public class SuccessConstants {
+    public static final String SUCCESS_CREATE = "success.create";
+    public static final String SUCCESS_SUBMIT = "success.submit";
+    public static final String SUCCESS_APPROVE = "success.approve";
+    public static final String SUCCESS_REJECT = "success.reject";
+    public static final String SUCCESS_CHECK = "success.check";
+
+    public static final String SUCCESS_UPDATE = "success.update";
+    public static final String SUCCESS_UPDATE_MODULE = "success.update.module";
+    public static final String SUCCESS_FIND_BY_ID = "success.find";
+    public static final String SUCCESS_STATUS_CHANGE = "success.status.change";
+    public static final String SUCCESS_FIND_LIST = "success.find.list";
+    public static final String SUCCESS_DELETE = "success.delete";
+
+    public static final String SUCCESS_TOGGLE = "success.toggle";
+    public static final String SUCCESS_UPLOAD = "success.upload";
+    public static final String SUCCESS = "success";
+    public static final String SUCCESS_PULLBACK = "success.pullback";
+    public static final String SUCCESS_PASSWORD_CHANGE = "success.password.change";
+    public static final String SUCCESS_PASSWORD_RESET_LINK = "success.password.reset.link";
+    public static final String SUCCESS_EMAIL_VERIFICATION = "success.email.verification";
+    public static final String SUCCESS_PASSWORD_RESET_SCHEDULED = "success.password.reset.scheduled";
+    public static final String UP_TO_DATE = "table.uptodate";
+    public static final String SUCCESS_SAVE = "success.save";
+}
